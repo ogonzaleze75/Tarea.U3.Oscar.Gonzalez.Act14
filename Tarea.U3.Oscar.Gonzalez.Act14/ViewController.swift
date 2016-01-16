01c7a00e917036b1dd074c996f36739e4de97c1a
ogonzaleze75 <ogonzaleze@outlook.com>
2016-01-16 18:23:58 -0300

    Initial Commit

---
+++ Tarea.U3.Oscar.Gonzalez.Act14/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tarea.U3.Oscar.Gonzalez.Act14
+//
+//  Created by Oscar on 16-01-16.
+//  Copyright © 2016 Oscar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
